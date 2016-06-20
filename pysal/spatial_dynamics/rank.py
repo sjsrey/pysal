cf284c36e19cdd1f2889da7d62fe57ce06e153c8
wei <weikang9009@gmail.com>
2016-06-20 14:23:56 -0700

    add a faster implementation to calculate neighbor set LIMA

--- pysal/spatial_dynamics/rank.py
+++ pysal/spatial_dynamics/rank.py
@@ -536,8 +536,6 @@ class Tau_Local_Neighbor:
     The equation for calculating neighbor set LIMA statistic can be
     found in Rey (2016) [Rey2014]_ Equation (16).
 
-
-
     Examples
     --------
     >>> import pysal as ps
@@ -640,6 +638,191 @@ class Tau_Local_Neighbor:
             self.tau_ln_pvalues = tau_ln_pvalues
 
 
+class Tau_Local_Neighbor_DecomFromSpatialTau:
+    """
+    Neighbor set LIMA.
+
+    Local concordance relationships between a focal unit and its
+    neighbors. A decomposition of local Tau into neighbor and
+    non-neighbor components.
+
+    Parameters
+    ----------
+    x              : array
+                     (n, ), first variable.
+    y              : array
+                     (n, ), second variable.
+    w              : W
+                     spatial weights object.
+    permutations   : int
+                     number of random spatial permutations for
+                     computationally based inference.
+
+    Attributes
+    ----------
+    n              : int
+                     number of observations.
+    tau_local       : array
+                     (n, ), local concordance (local version of the
+                     classic tau).
+    S              : array
+                     (n ,n), concordance matrix, s_{i,j}=1 if
+                     observation i and j are concordant, s_{i,
+                     j}=-1 if observation i and j are discordant,
+                     and s_{i,j}=0 otherwise.
+    tau_ln         : array
+                     (n, ), observed neighbor set LIMA values.
+    tau_ln_weights : array
+                     (n, ),
+    tau_ln_sim     : array
+                     (n, permutations), neighbor set LIMA values for
+                     permuted samples (if permutations>0).
+    tau_ln_pvalues : array
+                     (n, 1), pseudo p-values for observed neighbor
+                     set LIMA values under the null that concordance
+                     relationship between the focal state and itsn
+                     eighbors is not different from what could be
+                     expected from randomly distributed rank changes.
+
+
+    Notes
+    -----
+    The equation for calculating neighbor set LIMA statistic can be
+    found in Rey (2016) [Rey2014]_ Equation (16).
+
+    Examples
+    --------
+    >>> import pysal as ps
+    >>> import numpy as np
+    >>> np.random.seed(10)
+    >>> f = ps.open(ps.examples.get_path("mexico.csv"))
+    >>> vnames = ["pcgdp%d"%dec for dec in range(1940, 2010, 10)]
+    >>> y = np.transpose(np.array([f.by_col[v] for v in vnames]))
+    >>> r = y / y.mean(axis=0)
+    >>> regime = np.array(f.by_col['esquivel99'])
+    >>> w = ps.weights.block_weights(regime)
+    >>> res = Tau_Local_Neighbor_DecomFromSpatialTau(r[:,0],r[:,1],w,permutations=1000)
+    >>> res.tau_ln
+    array([-0.2       ,  1.        ,  1.        ,  1.        ,  0.33333333,
+            0.6       ,  0.6       , -0.5       ,  1.        ,  1.        ,
+            0.2       ,  0.33333333,  0.33333333,  0.5       ,  1.        ,
+            1.        ,  1.        ,  0.        ,  0.6       , -0.33333333,
+           -0.33333333, -0.6       ,  1.        ,  0.2       ,  0.        ,
+            0.2       ,  1.        ,  0.6       ,  0.33333333,  0.5       ,
+            0.5       , -0.2       ])
+    >>> res.tau_ln_weights
+    array([ 0.03968254,  0.03968254,  0.03174603,  0.03174603,  0.02380952,
+            0.03968254,  0.03968254,  0.03174603,  0.00793651,  0.03968254,
+            0.03968254,  0.02380952,  0.02380952,  0.03174603,  0.00793651,
+            0.02380952,  0.02380952,  0.03174603,  0.03968254,  0.02380952,
+            0.02380952,  0.03968254,  0.03174603,  0.03968254,  0.03174603,
+            0.03968254,  0.03174603,  0.03968254,  0.02380952,  0.03174603,
+            0.03174603,  0.03968254])
+    >>> (res.tau_ln * res.tau_ln_weights).sum() #global spatial tau
+    0.39682539682539675
+    >>> res1 = SpatialTau(r[:,0],r[:,1],w,permutations=1000)
+    >>> res1.tau_spatial
+    0.3968253968253968
+    >>> res.tau_ln_pvalues
+    array([[ 0.1958042 ],
+           [ 0.14885115],
+           [ 0.33266733],
+           [ 0.43056943],
+           [ 0.000999  ],
+           [ 0.12487512],
+           [ 0.17882118],
+           [ 0.002997  ],
+           [ 0.000999  ],
+           [ 0.25574426],
+           [ 0.01098901],
+           [ 0.000999  ],
+           [ 0.05894106],
+           [ 0.07692308],
+           [ 0.0959041 ],
+           [ 0.34465534],
+           [ 0.44955045],
+           [ 0.00899101],
+           [ 0.1008991 ],
+           [ 0.000999  ],
+           [ 0.00799201],
+           [ 0.05494505],
+           [ 0.13386613],
+           [ 0.03596404],
+           [ 0.01498501],
+           [ 0.0979021 ],
+           [ 0.14485514],
+           [ 0.24275724],
+           [ 0.02297702],
+           [ 0.33366633],
+           [ 0.15584416],
+           [ 0.000999  ]])
+
+
+    """
+
+    def __init__(self, x, y, w, permutations=0):
+
+        x = np.asarray(x)
+        y = np.asarray(y)
+        self.n = len(x)
+
+        w.transform = 'b'
+        self.tau_ln, self.tau_ln_weights = self._calc(x, y, w)
+
+        if permutations > 0:
+            tau_ln_sim = np.zeros((self.n,permutations))
+            tau_ln_pvalues = np.zeros((self.n,1))
+            for i in xrange(self.n):
+                yr = np.zeros_like(y)
+                xr = np.zeros_like(y)
+                rids = range(self.n)
+                rids.remove(i)
+                obs_i = self.tau_ln[i] #observed value of the ith neighbor set LIMA statistic
+
+                for j in xrange(permutations):
+                    pids = np.random.permutation(rids)
+                    xr[i] = x[i]
+                    xr[rids] = x[pids]
+                    yr[i] = y[i]
+                    yr[rids] = y[pids]
+                    tau_ln_sim[i, j] = self._calc(xr, yr, w, i)
+                larger = (tau_ln_sim[i] >= obs_i).sum()
+                if (permutations - larger) < larger:
+                    larger = permutations - larger
+                tau_ln_pvalues[i] = (larger+1.)/(1+permutations)
+            self.tau_ln_sim = tau_ln_sim
+            self.tau_ln_pvalues = tau_ln_pvalues
+
+    def _calc_r(self, xi, yi, xj, yj, w):
+        dx = xi - xj
+        dy = yi - yj
+        dxdy = dx * dy
+        if dxdy != 0:
+            if dxdy > 0.0:
+                return 1
+            else:
+                return -1
+        else:
+            return 0
+
+
+    def _calc(self, x, y, w, i=None):
+        if i != None:
+            iS_local = 0
+            for j in w.neighbors[i]:
+                iS_local += self._calc_r(x[i], y[i], x[j], y[j], w)
+            tau_ln = iS_local * 1.0 / w.cardinalities[i]
+            return tau_ln
+        else:
+            tau_ln = np.zeros(self.n)
+            tau_ln_weights = np.zeros(self.n)
+            for i in w.id_order:
+                iS_local = 0
+                for j in w.neighbors[i]:
+                    iS_local += self._calc_r(x[i], y[i], x[j], y[j], w)
+                tau_ln[i] = iS_local * 1.0 / w.cardinalities[i]
+                tau_ln_weights[i] = w.cardinalities[i]*1.0/w.s0
+            return tau_ln, tau_ln_weights
 
 
 class Tau_Local_Neighborhood:

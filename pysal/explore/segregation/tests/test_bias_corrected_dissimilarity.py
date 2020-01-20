2003694ac0038e7d2a06595c7f66d991410ac486
Serge <sjsrey@gmail.com>
2020-01-19 16:29:13 -0800

    decorate final segregation tests

--- pysal/explore/segregation/tests/test_bias_corrected_dissimilarity.py
+++ pysal/explore/segregation/tests/test_bias_corrected_dissimilarity.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 import pysal.lib
 import geopandas as gpd
 import numpy as np
@@ -6,6 +7,7 @@ from pysal.explore.segregation.aspatial import BiasCorrectedDissim
 
 
 class Bias_Corrected_Dissim_Tester(unittest.TestCase):
+    @pytest.mark.slow
     def test_Bias_Corrected_Dissim(self):
         s_map = gpd.read_file(pysal.lib.examples.get_path("sacramentot2.shp"))
         df = s_map[['geometry', 'HISP_', 'TOT_POP']]
--- pysal/explore/segregation/tests/test_local_multi_local_diversity.py
+++ pysal/explore/segregation/tests/test_local_multi_local_diversity.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 import pysal.lib
 import geopandas as gpd
 import numpy as np
@@ -6,6 +7,7 @@ from pysal.explore.segregation.local import MultiLocalDiversity
 
 
 class Multi_Local_Diversity_Tester(unittest.TestCase):
+    @pytest.mark.slow
     def test_Multi_Local_Diversity(self):
         s_map = gpd.read_file(pysal.lib.examples.get_path("sacramentot2.shp"))
         groups_list = ['WHITE_', 'BLACK_', 'ASIAN_','HISP_']

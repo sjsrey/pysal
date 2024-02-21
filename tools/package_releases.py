fa8b40c0781442a1667b6b552b265bc226e9f0e9
Serge Rey <sjsrey@gmail.com>
2024-02-21 15:55:50 -0800

    filter releases on date and rc/post tags

--- tools/package_releases.py
+++ tools/package_releases.py
@@ -1,10 +1,8 @@
-
 """
 Grab most recent releases tagged on Github for PySAL subpackages
 
 
 TODO
- - add logic to get all package releases (not just latest) within the meta-cycle
  - parse the highlights for each release and build up change log highlights for meta
  - build up detailed changes for meta
  - decide if this becomes a workflow
@@ -12,42 +10,105 @@ TODO
  - consolidate common tooling from releases.py and pakcage_releases.py
 
 """
-import pandas
-import os
 import json
-import urllib
+import os
 import re
-import yaml
-from urllib.request import urlopen
-from datetime import datetime, timedelta
-import requests
 import subprocess
+import urllib
+from datetime import datetime, timedelta
+from urllib.request import urlopen
+
+import pandas
+import requests
+import yaml
+
+packages = [
+    "access",
+    "esda",
+    "giddy",
+    "inequality",
+    "libpysal",
+    "mapclassify",
+    "mgwr",
+    "momepy",
+    "pointpats",
+    "segregation",
+    "spaghetti",
+    "spglm",
+    "spint",
+    "splot",
+    "spopt",
+    "spreg",
+    "spvcm",
+    "tobler",
+]
 
 
-packages = ["libpysal", "access", "esda", "giddy", "inequality", "pointpats",
-            "segregation", "spaghetti", "mgwr", "momepy", "spglm", "spint", "spreg", "spvcm",
-            "tobler", "mapclassify", "splot", "spopt"]
+def get_releases():
+    """Return information on all pysal package releases.
 
 
-dfs = {}
-logs = {}
-for package in packages:
-    fout = f"{package}.txt"
-    cmd = f"gh release list -R pysal/{package} > {fout}"
-    print(cmd)
-    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
-                   shell=True, check=True)
-    df = pandas.read_csv(fout, sep="\t", header=None, names=['Name', 'Type', "Label", 'Date'])
-    df['Date'] = pandas.to_datetime(df['Date'])
+    Returns
+    -------
+    dfs : pandas DataFrame
+    """
+    dfs = {}
+    logs = {}
+    for package in packages:
+        fout = f"{package}.txt"
+        cmd = f"gh release list -R pysal/{package} > {fout}"
+        print(cmd)
+        subprocess.run(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            shell=True,
+            check=True,
+        )
+        df = pandas.read_csv(
+            fout, sep="\t", header=None, names=["Name", "Type", "Label", "Date"]
+        )
+        df["Date"] = pandas.to_datetime(df["Date"])
 
-    dfs[package] = df
+        dfs[package] = df
 
-    #gh release view -R pysal/segregation
-    logfile = f'{package}.chglog'
-    cmd = f"gh release view -R pysal/{package} > {logfile}"
-    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
-                   shell=True, check=True)
-    with open(logfile, 'r') as lf:
-        logs[package] = lf.read()
+        # gh release view -R pysal/segregation
+        logfile = f"{package}.chglog"
+        cmd = f"gh release view -R pysal/{package} > {logfile}"
+        subprocess.run(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            shell=True,
+            check=True,
+        )
+        with open(logfile, "r") as lf:
+            logs[package] = lf.read()
+    dfs = pandas.concat(dfs)
+    dfs = dfs.reset_index()
+    dfs.rename(columns={"level_0": "Package"}, inplace=True)
+    return dfs
 
 
+def releases_since_date(date_string, df, exclude=["r", "post"]):
+    """Return releases since a specified date.
+
+    Parameters
+    ----------
+    date_string : str
+                Date to filter releases by
+    df : DataFrame
+          See get_releases
+    exclude : list
+          Exclude any release with a name containing specified strings
+
+    Returns
+    -------
+    df : DataFrame
+    """
+    for token in exclude:
+        dfs = df[~ds.Label.str.contains(token)]
+    date_string = pandas.to_datetime(date_string).tz_localize("UTC")
+    return df[df["Date"] >= date_string]
